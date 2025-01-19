daece6931b7829501a96d1cf05ba6fe0c76a8638
Kevin Michael <kmichaels@L-5CG4443W7F.>
2025-01-20 11:33:44 +1300

    batman

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+
+  description = "Installing language servers, formatters and related tools";
+
+  inputs = {
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  };
+
+  outputs = { self, nixpkgs }: let
+
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs { inherit system; };
+
+  in {
+
+    packages.${system}.default = pkgs.buildEnv {
+
+      name = "language-tools";
+
+      paths = with pkgs; [
+
+        carapace
+        pipx
+      	bun
+        nil
+        sqlfluff
+        ansible-language-server
+        # helix-gpt
+        markdown-oxide
+        prettierd
+        sqls
+        taplo
+        vale-ls
+        vale
+        yaml-language-server
+        lua-langauge-server
+        stylua
+
+      ];
+
+    };
+
+  };
+
+}
